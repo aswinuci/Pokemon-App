78e533c2d4254ebda76b86c269900bdb0c30325d
aswinuci <saswin@uci.edu>
2024-03-30 20:35:25 -0700

    Added detail page

---
+++ public/script.js
@@ -0,0 +1,192 @@
+const pokemons = [
+    "bulbasaur",
+    "ivysaur",
+    "venusaur",
+    "charmander",
+    "charmeleon",
+    "charizard",
+    "squirtle",
+    "wartortle",
+    "blastoise",
+    "caterpie",
+    "metapod",
+    "butterfree",
+    "weedle",
+    "kakuna",
+    "beedrill",
+    "pidgey",
+    "pidgeotto",
+    "pidgeot",
+    "rattata",
+    "raticate",
+    "spearow",
+    "fearow",
+    "ekans",
+    "arbok",
+    "pikachu",
+    "raichu",
+    "sandshrew",
+    "sandslash",
+    "nidoran-f",
+    "nidorina",
+    "nidoqueen",
+    "nidoran-m",
+    "nidorino",
+    "nidoking",
+    "clefairy",
+    "clefable",
+    "vulpix",
+    "ninetales",
+    "jigglypuff",
+    "wigglytuff",
+    "zubat",
+    "golbat",
+    "oddish",
+    "gloom",
+    "vileplume",
+    "paras",
+    "parasect",
+    "venonat",
+    "venomoth",
+    "diglett",
+    "dugtrio",
+    "meowth",
+    "persian",
+    "psyduck",
+    "golduck",
+    "mankey",
+    "primeape",
+    "growlithe",
+    "arcanine",
+    "poliwag",
+    "poliwhirl",
+    "poliwrath",
+    "abra",
+    "kadabra",
+    "alakazam",
+    "machop",
+    "machoke",
+    "machamp",
+    "bellsprout",
+    "weepinbell",
+    "victreebel",
+    "tentacool",
+    "tentacruel",
+    "geodude",
+    "graveler",
+    "golem",
+    "ponyta",
+    "rapidash",
+    "slowpoke",
+    "slowbro",
+    "magnemite",
+    "magneton",
+    "farfetchd",
+    "doduo",
+    "dodrio",
+    "seel",
+    "dewgong",
+    "grimer",
+    "muk",
+    "shellder",
+    "cloyster",
+    "gastly",
+    "haunter",
+    "gengar",
+    "onix",
+    "drowzee",
+    "hypno",
+    "krabby",
+    "kingler",
+    "voltorb",
+    "electrode",
+    "exeggcute",
+    "exeggutor",
+    "cubone",
+    "marowak",
+    "hitmonlee",
+    "hitmonchan",
+    "lickitung",
+    "koffing",
+    "weezing",
+    "rhyhorn",
+    "rhydon",
+    "chansey",
+    "tangela",
+    "kangaskhan",
+    "horsea",
+    "seadra",
+    "goldeen",
+    "seaking",
+    "staryu",
+    "starmie",
+    "mr-mime",
+    "scyther",
+    "jynx",
+    "electabuzz",
+    "magmar",
+    "pinsir",
+    "tauros",
+    "magikarp",
+    "gyarados",
+    "lapras",
+    "ditto",
+    "eevee",
+    "vaporeon",
+    "jolteon",
+    "flareon",
+    "porygon",
+    "omanyte",
+    "omastar",
+    "kabuto",
+    "kabutops",
+    "aerodactyl",
+    "snorlax",
+    "articuno",
+    "zapdos",
+    "moltres",
+    "dratini",
+    "dragonair",
+    "dragonite",
+    "mewtwo",
+    "mew"
+];
+
+function showSuggestions() {
+    const input = document.getElementById('pokemonInput');
+    const inputValue = input.value.toLowerCase();
+    const suggestions = document.getElementById('suggestions');
+    suggestions.innerHTML = '';
+  
+    // Filter fruits based on input value
+    const filteredPokemons = pokemons.filter(fruit => fruit.toLowerCase().startsWith(inputValue));
+    
+    // Display suggestions
+    filteredPokemons.forEach(pokemon => {
+      const suggestion = document.createElement('a');
+      suggestion.href = `/${pokemon}`;
+      suggestion.textContent = pokemon;
+      suggestions.appendChild(suggestion);
+    });
+  
+    // Show suggestion box
+    if (inputValue.length > 0 && filteredPokemons.length > 0) {
+      suggestions.style.display = 'block';
+    } else {
+      suggestions.style.display = 'none';
+    }
+}
+
+document.addEventListener('click', function(event) {
+    const suggestions = document.getElementById('suggestions');
+    if (!event.target.matches('#pokemonInput') && !event.target.matches('.suggestion-box a')) {
+      suggestions.style.display = 'none';
+    }
+});
+
+function setProgressBarWidth(statValue, statName) {
+  const progressBar = document.getElementById('statProgressBar' + statName);
+  if (progressBar) {
+      progressBar.style.width = statValue + '%';
+  }
+}
